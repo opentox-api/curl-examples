496d947f8745e73b4c0e876206820ab4042eccb6
Sopasakis Pantelis <p.sopasakis@gmail.com>
2013-10-06 17:41:26 +0200

    Update authenticate.sh

--- aa/authenticate.sh
+++ aa/authenticate.sh
@@ -3,5 +3,7 @@ export username=guest
 #Your password:
 export password=guest
 #Retrieve the token:
-export token=`curl -X POST -k 'https://opensso.in-silico.ch:443/auth/authenticate?uri=service=openldap' -d username=$username -d password=$password`
+export token=`curl -X POST -k \
+  'https://opensso.in-silico.ch:443/auth/authenticate?uri=service=openldap' \
+  -d username=$username -d password=$password`
 token=`echo $token | cut -c10-80`;
