ecf2e226078318cd21853294f94c056e75a6b1dd
alphaville <chvng@mail.ntua.gr>
2013-10-06 17:56:19 +0200

    Minor modification

--- aa/authenticate.sh
+++ aa/authenticate.sh
@@ -3,6 +3,9 @@
 export username=guest
 #Your password:
 export password=guest
-#Retrieve the token:
+#Acquire a token from the OpenSSO Server:
 export token=`curl -X POST -k 'https://opensso.in-silico.ch:443/auth/authenticate?uri=service=openldap' -d username=$username -d password=$password 2> /dev/null`
 token=`echo $token | cut -c10-80`;
+echo "[TOKEN] $token"
+#Invalidate the token
+curl -d subjectid=$token http://opensso.in-silico.ch/opensso/identity/logout
