2c3253abd9bf9aa8d1920d1589ca089a3a5d1e15
alphaville <chvng@mail.ntua.gr>
2013-10-06 17:33:49 +0200

    First commit

---
+++ aa/authenticate.sh
@@ -0,0 +1,7 @@
+#Your username:
+export username=guest
+#Your password:
+export password=guest
+#Retrieve the token:
+export token=`curl -X POST -k 'https://opensso.in-silico.ch:443/auth/authenticate?uri=service=openldap' -d username=$username -d password=$password`
+token=`echo $token | cut -c10-80`;
